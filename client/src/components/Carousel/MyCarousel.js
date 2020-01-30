ee855205581c0ffcca68e7f303776ac4a98b32f2
squarerobin <squarerobin@users.noreply.github.com>
2020-01-30 21:39:36 +0100

    save

--- client/src/components/Carousel/MyCarousel.js
+++ client/src/components/Carousel/MyCarousel.js
@@ -1,38 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
-  CarouselCaption,
- 
-} from 'reactstrap';
-import "./MyCarousel.css"
-import styled from 'styled-components';
-
+  CarouselCaption
+} from "reactstrap";
 
 const items = [
   {
-    src:
-      "client/src/components/Carousel/imagesdamian-patkowski-T-LfvX-7IVg-unsplash.jpg",
-    altText: "",
-    caption: "Be the change that you wish to see in the world. ― Mahatma Gandhi"
+    id: 1,
+    altText: "Slide 1",
+    caption: "Slide 1"
   },
   {
-    src: "https://www.fillmurray.com/1600/800",
-    altText: "",
-    caption:
-      "In three words I can sum up everything I've learned about life: it goes on. ― Robert Frost"
+    id: 2,
+    altText: "Slide 2",
+    caption: "Slide 2"
   },
   {
-    src: "https://www.fillmurray.com/1600/800",
-    altText: "",
-    caption:
-      "If you tell the truth, you don't have to remember anything. ― Mark Twain"
+    id: 3,
+    altText: "Slide 3",
+    caption: "Slide 3"
   }
 ];
 
-const Example = (props) => {
+const Example = props => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -40,66 +33,66 @@ const Example = (props) => {
     if (animating) return;
     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
-  }
+  };
 
   const previous = () => {
     if (animating) return;
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
-  }
+  };
 
-  const goToIndex = (newIndex) => {
+  const goToIndex = newIndex => {
     if (animating) return;
     setActiveIndex(newIndex);
-  }
+  };
 
-  //styles for slides 
-
- const ReadMoreButton = styled.button`
-     
-  
-  `
-
-  const slides = items.map((item) => {
+  const slides = items.map(item => {
     return (
-      <CarouselItem 
+      <CarouselItem
+        className="custom-tag"
+        tag="div"
+        key={item.id}
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={item.src}
       >
-        <img src={item.src} alt={item.altText} />
-        <CarouselCaption color={`green`} captionText={item.caption} captionHeader={item.caption} />
-        
-        </CarouselItem>
+        <CarouselCaption
+          className="text-danger"
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
     );
   });
 
- 
-
   return (
-    <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-      <CarouselIndicators
-      className="carousel-indicator"
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-      <ReadMoreButton class="readme-button" >  
-        <span>Read More</span>
-      </ReadMoreButton>
-    </Carousel>
+    <div>
+      <style>
+        {`.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: black;
+            }`}
+      </style>
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
+    </div>
   );
-}
+};
 
 export default Example;
