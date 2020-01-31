f27cb97e0faf2167fe55f1d22b2fc1265b62fc4e
squarerobin <squarerobin.82@gmail.com>
2020-01-31 05:53:01 +0100

    I have to finish the button, carousel done
    I wanna work on the navbar too

--- client/src/components/Carousel/MyCarousel.js
+++ client/src/components/Carousel/MyCarousel.js
@@ -6,26 +6,34 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from "reactstrap";
+import "./MyCarousel.css";
+import styled from "styled-components";
 
 const items = [
   {
-    id: 1,
-    altText: "Slide 1",
-    caption: "Slide 1"
+    src: "./images/1.jpg",
+    altText: "",
+    caption:
+      "Be the change that you wish to see in the world. ― Mahatma Gandhi",
+    button: ""
   },
   {
-    id: 2,
-    altText: "Slide 2",
-    caption: "Slide 2"
+    src: "./images/2.jpg",
+    altText: "",
+    caption:
+      "In three words I can sum up everything I've learned about life: it goes on. ― Robert Frost",
+    button: ""
   },
   {
-    id: 3,
-    altText: "Slide 3",
-    caption: "Slide 3"
+    src: "./images/3.jpg",
+    altText: "",
+    caption:
+      "If you tell the truth, you don't have to remember anything. ― Mark Twain",
+    button: ""
   }
 ];
 
-const Example = props => {
+const MyCarousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -46,17 +54,24 @@ const Example = props => {
     setActiveIndex(newIndex);
   };
 
+  //styles for slides
+
+  /*TODO modify colors, check palette*/
+  const ReadMoreButton = styled.button`
+    background-color: orange;
+    color: white;
+  `;
+
   const slides = items.map(item => {
     return (
       <CarouselItem
-        className="custom-tag"
-        tag="div"
-        key={item.id}
+        class="carousel-item"
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
+        key={item.src}
       >
+        <img src={item.src} alt={item.altText} />
         <CarouselCaption
-          className="text-danger"
           captionText={item.caption}
           captionHeader={item.caption}
         />
@@ -65,34 +80,26 @@ const Example = props => {
   });
 
   return (
-    <div>
-      <style>
-        {`.custom-tag {
-              max-width: 100%;
-              height: 500px;
-              background: black;
-            }`}
-      </style>
-      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-        <CarouselIndicators
-          items={items}
-          activeIndex={activeIndex}
-          onClickHandler={goToIndex}
-        />
-        {slides}
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={previous}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={next}
-        />
-      </Carousel>
-    </div>
+    <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+      <CarouselIndicators
+        
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
   );
 };
 
-export default Example;
+export default MyCarousel;
