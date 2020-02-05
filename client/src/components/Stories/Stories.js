eedb56d0cbb6beb22d784052b2e75bd7d23e252e
squarerobin <squarerobin.82@gmail.com>
2020-02-05 06:40:55 +0100

    bug fixed, getting data from API

---
+++ client/src/components/Stories/Stories.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Story from "./Story";
+
+const Stories = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://refu-stories-backend.herokuapp.com/api/stories`)
+      .then(res => setData(res.data))
+      .catch(err => `Houston we have an error: ${err}`);
+  }, []);
+
+  console.log(data);
+
+  return data.map(story => (
+    <div>
+      <Story str={story} />
+    </div>
+  ));
+};
+
+export default Stories;
--- client/src/components/Stories/Story.js
+++ client/src/components/Stories/Story.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Story.scss";
 
-function Story() {
+function Story(props) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://refu-stories-backend.herokuapp.com/api/stories`)
-      .then(res => setData(res.data[0]))
+      .then(res => setData(res.data))
       .catch(err => `Houston we have an error: ${err}`);
   }, []);
   console.log(data);
 
-return (
+ return (
       <div className="story">
-        <h2 key={data.id}>{data.title}</h2>
-        <p key={data.id}>{data.contents}</p>
+        <h2 >{props.str.title}</h2>
+        <p >{props.str.contents}</p>
       </div>
-)
+ )
+
+
+
   
 }
 
