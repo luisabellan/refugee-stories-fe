235fd867ec363f13c8695eb81082e6eca27ece33
squarerobin <squarerobin.82@gmail.com>
2020-02-05 08:44:29 +0100

    Stories.js and Story.js - done

--- client/src/components/Stories/Story.js
+++ client/src/components/Stories/Story.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "./Story.css";
-import "../../App.css";
+
 
 function Story(props) {
   const [data, setData] = useState([]);
